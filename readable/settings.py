f6da787458290dfb91bb8ce5493de308ac7e505e
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2025-11-11 12:05:46 +0000

    Configure SECRET_KEY for local host and deployment

--- readable/settings.py
+++ readable/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f9_i^^lz8xpy3(6bv$o5a*^vri!^kerrg1vzo=u3c*k+95c+)w'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
