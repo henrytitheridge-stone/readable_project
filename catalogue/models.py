63472ba55817dfa7de497936d0b6be5c88bd6208
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2025-11-11 13:35:53 +0000

    Create, migrate and register the Review model

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+RATING = ((1, "1/5 stars"),
+          (2, "2/5 stars"),
+          (3, "3/5 stars"),
+          (4, "4/5 stars"),
+          (5, "5/5 stars"))
+
 
 # Create your models here.
 class Book(models.Model):
@@ -23,3 +29,23 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} | by {self.author}"
+
+
+class Review(models.Model):
+    """
+    Stores a single review related to :model: `auth.User`
+    and :model: `catalogue.Book`
+    """
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviewer")
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    rating = models.IntegerField(choices=RATING)
+    added_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-added_on"]
+
+    def __str__(self):
+        return f"{self.rating} for {self.book.title} | reviewed by {self.reviewer}"
