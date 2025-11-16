d1266c0dd60011664b9d0dede67f6b9c957e2ab3
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2025-11-16 11:30:27 +0000

    Wire up delete_review functionality and confirmation modal

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # path('', views.homepage, name='home'),
     path('<slug:slug>/', views.book_detail, name='book_detail'),
     path('<slug:slug>/edit_review/<int:review_id>', views.review_edit, name='review_edit'),
+    path('<slug:slug>/delete_review/<int:review_id>', views.review_delete, name='review_delete'),
 ]
--- static/js/reviews.js
+++ static/js/reviews.js
@@ -5,6 +5,10 @@ const reviewRating = document.getElementById("id_rating");
 const reviewForm = document.getElementById("reviewForm");
 const submitButton = document.getElementById("submitButton");
 
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
 /**
 * Initializes edit functionality for the provided edit buttons.
 * 
@@ -29,5 +33,23 @@ for (let button of editButtons) {
 
     submitButton.innerText = "Update";
     reviewForm.setAttribute("action", `edit_review/${reviewId}`);
+  });
+}
+
+/**
+* Initializes deletion functionality for the provided delete buttons.
+* 
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the 
+* deletion endpoint for the specific comment.
+* - Displays a confirmation modal (`deleteModal`) to prompt 
+* the user for confirmation before deletion.
+*/
+for (let button of deleteButtons) {
+  button.addEventListener("click", (e) => {
+    let reviewId = e.target.getAttribute("review_id");
+    deleteConfirm.href = `delete_review/${reviewId}`;
+    deleteModal.show();
   });
 }
